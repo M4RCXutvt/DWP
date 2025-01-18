dd8babc1739a572b183c4864a9efca3d983275cf
M4RCXutvt <133244133+M4RCXutvt@users.noreply.github.com>
2025-01-18 10:01:21 -0600

    lab2
    actividades del lab2

---
+++ MarcoJavaEdition/src/main/java/org/example/clase1/Variables.java
@@ -0,0 +1,22 @@
+package org.example.clase1;
+
+public class Variables {
+    public static void main(String[] args) {
+        //Variables
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+
+        //Inicializar una variable
+        int initValue;
+        initValue = 100;
+        System.out.println(initValue);
+
+        //Variable LONG
+        long hugeValue = 100000000L;
+        System.out.println(hugeValue);
+
+        //Variable Flotante
+        float floatVar = 3.14F;
+        System.out.println(floatVar);
+    }
+}
